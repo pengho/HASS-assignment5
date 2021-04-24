3140152204482f30fa1919f59be2ed9ddb2fb2df
rach codes <73816438+ALittleOf@users.noreply.github.com>
2021-04-24 23:17:16 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var svg = d3.select("#my_dataviz")
     .attr("width", width)
     .attr("height", height)
 
+
+
 // Read data FY2021
 d3.csv("./Untitled3.csv", function(data) {
 
@@ -19,6 +21,10 @@ d3.csv("./Untitled3.csv", function(data) {
   var color = d3.scaleOrdinal()
     .domain(["Asia", "Europe", "Africa", "Oceania"])
     .range(d3.schemeSet2);
+
+  let xPosition = d3.scaleOrdinal()
+    .domain([0, 1, 2])
+    .range([600, 900, 1200]);
 
   // Size scale for countries
   var size = d3.scaleLinear()
@@ -76,14 +82,16 @@ d3.csv("./Untitled3.csv", function(data) {
 
   // Features of the forces applied to the nodes:
   var simulation = d3.forceSimulation()
-      .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+      .nodes(data)
+      .force("x", d3.forceX().strength(0.5).x( d => xPosition(d.class) ))
+      .force("y", d3.forceY().strength(0.2).y( height /2 ))
+      // .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
       .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
       .force("collide", d3.forceCollide().strength(.5).radius(function(d){ return (size(d.value)+3) }).iterations(1)) // Force that avoids circle overlapping
 
   // Apply these forces to the nodes and update their positions.
   // Once the force algorithm is happy with positions ('alpha' value is low enough), simulations will stop.
   simulation
-      .nodes(data)
       .on("tick", function(d){
         node
             .attr("cx", function(d){ return d.x; })
